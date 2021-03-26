0f4c4c8d601f3aa13feb24b19b3b2591747aca0e
tiavinaralandison@gmail.com <tiavinaralandison@gmail.com>
2021-03-26 15:57:12 +0100

    début de l'intégration

--- index.php
+++ index.php
@@ -1,3 +1,54 @@
 <?php include "header.php" ?>
 
+<div class="container">
+
+ 
+<!-- Début de la navbar -->
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+        <img src="images/logo.png" alt="">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <span class="navbar-nav me-auto mb-2 mb-lg-0">
+      </span>
+      <span class="">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Find a doctor</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Apps</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Testimonials</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">About us</a>
+            </li>
+        </ul>
+      </span>
+    </div>
+  </div>
+</nav>
+
+<!-- Fin de la navbar -->
+
+
+    <div class="row">
+        <div class="col-md-6 col-md-sm-12"></div>
+        <div class="col-md-6 col-md-sm-12"><img src="images/illustration-header.png" alt="">
+        </div>
+    </div>
+</div>
+
+
+
 <?php include "footer.php" ?>
