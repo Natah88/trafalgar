7de0e6d3aee6f6951116f1c50a37428a31c6512b
tiavinaralandison@gmail.com <tiavinaralandison@gmail.com>
2021-04-01 16:00:38 +0200

    Résolution du problème de cards

--- index.php
+++ index.php
@@ -1,54 +1,230 @@
 <?php include "header.php" ?>
 
 <div class="container">
-
- 
 <!-- Début de la navbar -->
-
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">
-        <img src="images/logo.png" alt="">
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarText">
-      <span class="navbar-nav me-auto mb-2 mb-lg-0">
-      </span>
-      <span class="">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" href="#">Find a doctor</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" href="#">Apps</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" href="#">Testimonials</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" href="#">About us</a>
-            </li>
-        </ul>
-      </span>
-    </div>
-  </div>
-</nav>
-
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="images/logo.png" alt="logo-de-trafalgar">  
+            </a>
+        
+            <button class="navbar-toggler" type="button"        data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        
+            <div class="collapse navbar-collapse" id="navbarText">
+                <span class="navbar-nav me-auto mb-2 mb-lg-0"></span>
+            
+                <span class="">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Find a doctor</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Apps</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Testimonials</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">About us</a>
+                        </li>
+                    </ul>
+                </span>
+            </div>
+        </div>
+    </nav>
+</div>
 <!-- Fin de la navbar -->
 
+    <div class="d-flex justify-content-right p-5">
+        <img src="images\element.png" alt="" class="">
+    </div>
 
-    <div class="row">
-        <div class="col-md-6 col-md-sm-12"></div>
-        <div class="col-md-6 col-md-sm-12"><img src="images/illustration-header.png" alt="">
+<div class="contenair">
+    <div class="row mb-5">
+        <div class="d-flex align-items-center col-md-6 col-md-sm-12">
+            <div class="row">
+                <div class="presentation col-md-8 col-sm-12">
+                    <h1><b>Virtual healthcare for you.</b></h1>
+                    <p>Trafalgar provides progressive, and affordable healthcare accessible on mobile and online for everyone.</p>
+                </div>
+            </div>
         </div>
+        
+        <div class="col-md-6 col-md-sm-12">
+            <img src="images/illustration-header.png" alt="illustration-de-presentation">
+        </div>
+
+        <h2 class="center">Our services</h2>
+        <p class="center"><img src="images\underline.png" alt="" class=""></p>
+
+        <div class="container">
+            <div class="text mb-5">
+                <p class="center"> We provide to you the best choiches for you. Adjust it to your health needs and make sure your undergo treatment <br> with our highly qualified doctors you can consult with us which type of service is suitable for your health
+                </p>
+            </div>
+        
+            <div class="bord container">
+                <div class="row mb-5">
+                    <div class="text-center col">
+                        <img src=images\slice-search-a-doctor.png alt="illustration-pour-rechercher">
+                        <h3 class="services">Search doctor</h3>
+                        <p>Choose your doctor from <br>thousands of specialist,<br> general, and trusted hospitals.
+                        </p>
+                    </div>
+
+                    <div class="text-center col">
+                        <img src=images\online-pharmacy.png alt="illustration-pour-pharmacie">
+                        <h3 class="services">Online pharmacy</h3>
+                        <p>Buy  your medicines with our <br> mobile application with <br>a simple delivery system.
+                        </p>
+                    </div>
+        
+
+                    <div class="text-center col">
+                        <img src=images\consultation.png alt="illustration-pour-consultation">
+                        <h3 class="services">Consultation</h3>
+                        <p>Free consultation with our <br> trusted doctors and get<br> the best recomendations.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="element-2"><img src="images\element-2.png" alt="" ></div>
+
+            <div class="bord container">
+                <div class="row">
+                    <div class="text-center col">
+                        <img src=images\details-info.png alt="illustration-pour-details-d-info">
+                        <h3 class="services">Details info</h3>
+                        <p>Free consultation with <br>our trusted doctors and get<br>the best recomendations.
+                        </p>
+                    </div>
+
+                    <div class="text-center col">
+                        <img src=images\emergency-care.png alt="illustration-pour-le-soin-en-urgence">
+                        <h3 class="services">Emergency care</h3>
+                        <p>You can get 24/7 urgent<br> care for yourself or your<br>children and your lovely family
+                        </p>
+                    </div>
+
+                    <div class="text-center col">
+                        <img src=images\tracking.png alt="illustration-pour-le-suivi">
+                        <h3 class="services">Tracking</h3>
+                        <p>Track and save your medical<br> history and health data.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <p class="center button d-grid gap-1 col-1 mx-auto">
+        <button type="button" class="btn btn-outline-primary">Learn more</button>
+    </p>
+    
+    <div class="row mb-5">
+        <div class="d-flex align-items-center col-md-6 col-md-sm-12">
+            <img src="images\illustration-body.png" alt="illustration-dans-body">
+        </div>
+        <div class="body col-md-6 col-sm-12 p-5">
+            <h1>Leading healthcare providers.</h1>
+            <p>Trafalgar provides progressive,<br> and affordable healthcare, <br> accessible on mobile and online for everyone.<br> To us, it’s not just work.<br> We take pride in the solutions we deliver.
+            </p>
+        </div>
+    </div>
+
+    <div class="row mb-5">
+        <div class="download col-md-6 col-sm-12">
+            <h1>Download our mobile apps.</h1>
+            <p>Our dedicated patient engagement <br>app and web portal allow you to access  <br>information instantaneously <br>(no tedeous form, long calls, or <br>administrative hassle) and securely.</p>
+        </div>
+        <div class="d-flex align-items-center col-md-6 col-md-sm-12"><img src="images\illustration-body-2.png" alt="illustration-dans-body">
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="card mb-5 p-5 card-border-radius">
+            <div class="card-body linear-gradient card-border-radius">
+                <h3 class="text-center card-title"><b>What our customer are saing
+                </b></h3>
+
+                <div class="element 4">
+                    <img src="images\element.png" alt="" class="">
+                </div>
+                
+                <div class="row">
+                    <div class="mb-8 col-md-1 col-sm-0"></div>
+                    <div class="pic col-md-2 col-sm-6">
+                        <img src="images\image-customer.png" alt="image-du-client">
+                    </div>
+                    <div class="mb-5 col-md-2 col-sm-6">
+                        <p class="card-identity">
+                            Edward Newgate <br>Founder Circle
+                        </p>
+                    </div>
+                    <div class="mb-5 col-md-1 col-sm-0">
+                    </div>
+                    <div class="mb-5 col-md-4 col-sm-12">            
+                        <div class="card-text">
+                            "Our dedicated patient engagement app and web portal allow you to access information instantaneously(no tedeous form, long calls, or administrative hassle) and securely”.
+                        </div>
+                    </div>
+                    <div class="mb-5 col-md-1 col-sm-0"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="previous-next">
+        <img src="images\previous.png" alt="">
+        <img src="images\quatrepoint.png" alt="">
+        <img src="images\next.png" alt="">
     </div>
 </div>
 
+<div class="check-list text-center">
+        <h2><b>Check out latest article</b></h2>
+        <span class="border-bottom-line"></span>
+</div>
 
+<div class="container">
+        <div class="row mb-5">
 
+            <div class="col-md-4 col-sm-12">
+                <img src="images\img-col-1.png" alt="">
+                <div class="text-card">
+                    <h4>Disease detection, check up in the laboratory</h4>
+                    <p>In this case, the role of the health laboratory is very important to do a disease detection.</p>
+                </div>
+            </div>
+
+            <div class="col-md-4 col-sm-12">
+                <img src="images\img-col-2.png" alt="">
+                <div class="text-card">
+                    <h4>Herbal medicines that are safe for consumption</h4>
+                    <p>Herbal medicine is very widely used at this time because of its very good for your health.</p>
+                </div>
+            </div>
+
+            <div class="col-md-4 col-sm-12">
+                <img src="images\img-col-3.png" alt="">
+                <div class="text-card">
+                    <h4>Natural care for healthy facial skin</h4>
+                    <p>A healthy lifestyle should start from now and also for your skin health.There are some</p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <p class="button d-grid gap-1 col-1 mx-auto">
+        <button type="button" class="btn btn-outline-primary">View all</button>
+    </p>
+</div>>
 <?php include "footer.php" ?>
+
