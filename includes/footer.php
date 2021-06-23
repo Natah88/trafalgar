6ee66cebc02534401794f511d126fe058cb9916e
Tiavina <79489166+Tiah7@users.noreply.github.com>
2021-06-23 10:28:53 +0200

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,5 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script> 
-</body>
-</html>
+
 
     <div class="container-fluid">
         <div class="card-body linear-gradient">
@@ -41,7 +40,8 @@
             </div>
         </div>
     </div>
-
+</body>
+</html>
 
 
 
